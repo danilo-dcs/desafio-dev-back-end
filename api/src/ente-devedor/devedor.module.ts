44c4f8027b890e58906b171dcedf0610e4a37f8b
Danilo <danilopktcastro@gmail.com>
2021-08-24 21:42:28 -0300

    Service e Controller do Pagamento implementados

--- api/src/ente-devedor/devedor.module.ts
+++ api/src/ente-devedor/devedor.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DevedorController } from './devedor.controller';
+import { EnteDevedorEntity } from './devedor.entity';
 import { DevedorService } from './devedor.service';
 
 @Module({
+
+  imports: [
+    TypeOrmModule.forFeature([
+      EnteDevedorEntity
+    ])
+  ],
+
   controllers: [DevedorController],
   providers: [DevedorService]
 })
--- api/src/pagamento/pagamento.module.ts
+++ api/src/pagamento/pagamento.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PagamentoService } from './pagamento.service';
 import { PagamentoController } from './pagamento.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PagamentoEntity } from './pagamento.entity';
 
 @Module({
+
+  imports: [
+    TypeOrmModule.forFeature([
+      PagamentoEntity
+    ])
+  ],  
+
   providers: [PagamentoService],
   controllers: [PagamentoController]
 })
